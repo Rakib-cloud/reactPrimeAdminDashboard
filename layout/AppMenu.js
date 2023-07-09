030d2227b1be721efe4cd0abee2b3ee20d512464
Md .Rakibul Islam <57381896+Rakib-cloud@users.noreply.github.com>
2023-07-09 14:30:29 +0600

    added

---
+++ layout/AppMenu.js
@@ -0,0 +1,161 @@
+import React, { useContext } from 'react';
+import AppMenuitem from './AppMenuitem';
+import { LayoutContext } from './context/layoutcontext';
+import { MenuProvider } from './context/menucontext';
+import Link from 'next/link';
+
+const AppMenu = () => {
+    const { layoutConfig } = useContext(LayoutContext);
+
+    const model = [
+        {
+            label: 'Home',
+            items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+        },
+        {
+            label: 'UI Components',
+            items: [
+                { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
+               
+                { label: 'Float Label', icon: 'pi pi-fw pi-bookmark', to: '/uikit/floatlabel' },
+                
+                
+                { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
+                
+                { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
+                { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
+                { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
+                { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
+                { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu', preventExact: true },
+                
+                
+                { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
+               
+            ]
+        },
+        {
+            label: 'Orders',
+            items: [
+               
+                { label: 'All Blocks', icon: 'pi pi-fw pi-globe', url: 'https://blocks.primereact.org', target: '_blank' }
+            ]
+        },
+        {
+            label: 'Utilities',
+            items: [
+                
+                { label: 'PrimeFlex', icon: 'pi pi-fw pi-desktop', url: 'https://www.primefaces.org/primeflex/', target: '_blank' }
+            ]
+        },
+        {
+            label: 'Pages',
+            icon: 'pi pi-fw pi-briefcase',
+            to: '/pages',
+            items: [
+        
+                {
+                    label: 'Auth',
+                    icon: 'pi pi-fw pi-user',
+                    items: [
+                        {
+                            label: 'Login',
+                            icon: 'pi pi-fw pi-sign-in',
+                            to: '/auth/login'
+                        },
+                        {
+                            label: 'Error',
+                            icon: 'pi pi-fw pi-times-circle',
+                            to: '/auth/error'
+                        },
+                        {
+                            label: 'Access Denied',
+                            icon: 'pi pi-fw pi-lock',
+                            to: '/auth/access'
+                        }
+                    ]
+                },
+                {
+                    label: 'Crud',
+                    icon: 'pi pi-fw pi-pencil',
+                    to: '/pages/crud'
+                },
+                {
+                    label: 'Timeline',
+                    icon: 'pi pi-fw pi-calendar',
+                    to: '/pages/timeline'
+                },
+                {
+                    label: 'Not Found',
+                    icon: 'pi pi-fw pi-exclamation-circle',
+                    to: '/pages/notfound'
+                },
+                {
+                    label: 'Empty',
+                    icon: 'pi pi-fw pi-circle-off',
+                    to: '/pages/empty'
+                }
+            ]
+        },
+        {
+            label: 'Hierarchy',
+            items: [
+                {
+                    label: 'Submenu 1',
+                    icon: 'pi pi-fw pi-bookmark',
+                    items: [
+                        {
+                            label: 'Submenu 1.1',
+                            icon: 'pi pi-fw pi-bookmark',
+                            items: [
+                                { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
+                                { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
+                                { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
+                            ]
+                        },
+                        {
+                            label: 'Submenu 1.2',
+                            icon: 'pi pi-fw pi-bookmark',
+                            items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
+                        }
+                    ]
+                },
+                {
+                    label: 'Submenu 2',
+                    icon: 'pi pi-fw pi-bookmark',
+                    items: [
+                        {
+                            label: 'Submenu 2.1',
+                            icon: 'pi pi-fw pi-bookmark',
+                            items: [
+                                { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
+                                { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
+                            ]
+                        },
+                        {
+                            label: 'Submenu 2.2',
+                            icon: 'pi pi-fw pi-bookmark',
+                            items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
+                        }
+                    ]
+                }
+            ]
+        }
+       
+    ];
+
+    return (
+        <MenuProvider>
+            <ul className="layout-menu">
+                {model.map((item, i) => {
+                    return !item.seperator ? <AppMenuitem item={item} root={true} index={i} key={item.label} /> : <li className="menu-separator"></li>;
+                })}
+
+                {/* <Link href="https://blocks.primereact.org" target="_blank" style={{ cursor: 'pointer' }}>
+                    <img alt="Prime Blocks" className="w-full mt-3" src={`/layout/images/banner-primeblocks${layoutConfig.colorScheme === 'light' ? '' : '-dark'}.png`} />
+                </Link> */}
+            </ul>
+        </MenuProvider>
+    );
+};
+
+export default AppMenu;
